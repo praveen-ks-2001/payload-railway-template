ddc13f1d92ca5fe38bb7c34bb88679b52adaae14
pkdotsingh <praveenaman2001@gmail.com>
2026-03-21 00:25:28 +0530

    first commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -12,6 +12,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000', 
   admin: {
     user: Users.slug,
     importMap: {
@@ -26,8 +27,11 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
-    },
+    connectionString: process.env.DATABASE_URL || '',
+    ssl: process.env.DATABASE_SSL === 'true'
+      ? { rejectUnauthorized: false }
+      : false,
+  },
   }),
   sharp,
   plugins: [],
