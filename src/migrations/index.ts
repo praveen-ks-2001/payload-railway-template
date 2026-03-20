bd1a4d7c52b6081a41f445d23acd49c235fc46b6
pkdotsingh <praveenaman2001@gmail.com>
2026-03-21 03:53:37 +0530

    fix: add prodMigrations to auto-create DB schema on startup

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260320_222229 from './20260320_222229';
+
+export const migrations = [
+  {
+    up: migration_20260320_222229.up,
+    down: migration_20260320_222229.down,
+    name: '20260320_222229'
+  },
+];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import * as migrations from './migrations'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -32,6 +33,7 @@ export default buildConfig({
         ? { rejectUnauthorized: false }
         : false,
     },
+    prodMigrations: migrations,
   }),
   sharp,
   plugins: [],
